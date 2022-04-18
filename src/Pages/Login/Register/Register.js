d482abb3498136955ab453d78de3e7d8638f2532
emon al hasan <emonalhasan41@gmail.com>
2022-04-18 16:23:01 +0600

    Env variable

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 const Register = () => {
+    const navigate=useNavigate();
+    const navigateLogin=()=>{
+        navigate('/login')
+    }
+    const handleRegister=event=>{
+        event.preventDefault();
+        const name=event.target.name.value;
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+    }
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='register-form'>
+            <h2 className='text-center text-info'>Please Register</h2>
+            <form onSubmit={handleRegister} >
+            <input type="text" name="name" id="" placeholder='your name' />
+            <br />
+                <input type="email" name="email" id=""placeholder='your email'required />
+                <br />
+                <input type="password" name="password" id="" placeholder='your password' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already Have Account?<Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}> Please Login</Link></p>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import{getAuth} from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWmKtDhXhQQJvWis4J19eqfFdm9v4UN3k",
-  authDomain: "simple-gym-trainer.firebaseapp.com",
-  projectId: "simple-gym-trainer",
-  storageBucket: "simple-gym-trainer.appspot.com",
-  messagingSenderId: "399812034561",
-  appId: "1:399812034561:web:5c84efe90f981b54aa8000"
+  apiKey:process.env.REACT_APP_apiKey, 
+  authDomain:process.env.REACT_APP_authDomain, 
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket ,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId, 
 };
 
 // Initialize Firebase
