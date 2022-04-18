c94252ec2635e6aa385bf37bedeba74287a17665
emon al hasan <emonalhasan41@gmail.com>
2022-04-18 15:19:55 +0600

    Redmie

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -9,15 +9,16 @@ const Blogs = () => {
             </h4>
             <h3 className='text-center text-info mt-5 mb-3' >Q:Why are you using firebase? What other options do you have to implement authentication?</h3>
             <h4 className='text-center'>Ans::Firebase Authentication helps to building secure sign-in for any system easy. This  the solution supports email and passowrd, phone auth, & mult-platform login.This helps to make System more secure and useable.
-            1.Password base Auhentication.
-            2.Microsoft Authentication provider.
-            3.OATh Software tokens base authentication.
+           1.Back4App are alternative option.
+           2.Parse are alternative option.
+           3.AWS Amplify alternative option.
+           4.appwrite are alternative option 
 
 
 
             </h4>
             <h3 className='text-center text-info mt-5 mb-3'>Q:What other services does firebase provide other than authentication?</h3>
-            <h4 className='text-center'>Ans::Firebase Authentication does backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app.</h4>
+            <h4 className='text-center'>Ans::Firebase Authentication does backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users  for the app.Just like Heroku,Appwrite,Hoodie</h4>
         </div>
     );
 };
