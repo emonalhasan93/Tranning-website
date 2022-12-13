29d36048c8d7e2a165cd504e040f2e1ddc79a8a7
emon al hasan <emonalhasan41@gmail.com>
2022-12-13 09:02:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -25,15 +25,11 @@ function App() {
    <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
    <Route path='/login' element={<Login></Login>}></Route>
    <Route path='/register' element={<Register></Register>}></Route>
-   <Route path='/checkout' element={
-     <RequireAuth>
-       <Checkout></Checkout>
-     </RequireAuth>
-   }></Route>
+ 
    <Route path='/blogs' element={<Blogs></Blogs>}></Route>
    <Route path='*' element={<NotFound></NotFound>}></Route>
  </Routes>
-<Footer></Footer>
+
     </div>
   );
 }
